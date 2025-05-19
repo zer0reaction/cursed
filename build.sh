504d289c68f968693b974cc376c6a9880df75dbf
zer0reaction <boltgopnik@gmail.com>
2025-05-19 12:28:08 +0300

    Added bin folder and fixed .gitignore

--- build.sh
+++ build.sh
@@ -4,17 +4,22 @@ CC='gcc'
 CFLAGS='-Wall -Wextra -pedantic -std=c99 -fsanitize=address,undefined -pipe'
 LDFLAGS='-lncursesw'
 
-OBJ='main.o utility.o buffer.o editor.o'
+OBJ='bin/main.o bin/utility.o bin/buffer.o bin/editor.o'
+
+set -e
 
 if [ ! -f src/config.h ]; then
     cp src/config.def.h src/config.h
 fi
 
+mkdir -p bin
+echo '*' > bin/.gitignore
+
 set -xe
 
-$CC $CFLAGS -c -o main.o    src/main.c
-$CC $CFLAGS -c -o utility.o src/utility.c
-$CC $CFLAGS -c -o buffer.o  src/buffer.c
-$CC $CFLAGS -c -o editor.o  src/editor.c
+$CC $CFLAGS -c -o bin/main.o    src/main.c
+$CC $CFLAGS -c -o bin/utility.o src/utility.c
+$CC $CFLAGS -c -o bin/buffer.o  src/buffer.c
+$CC $CFLAGS -c -o bin/editor.o  src/editor.c
 
 $CC $CFLAGS -o cursed $OBJ $LDFLAGS
